37191f767400cefb8af9fcf3f2b46232899657e3
sadaj <sadaj@HALILLUCI>
2025-05-14 10:40:59 +0300

    Add project files.

---
+++ Sube1.EmlakciLib/KiralikEv.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sube1.EmlakciLib
+{
+    public class KiralikEv : Ev
+    {
+        public KiralikEv()
+        {
+
+        }
+
+        public KiralikEv(double kira, double depozito, int odasayisi, string semt, int katno, double alan):base(odasayisi,katno,alan,semt)
+        {
+            this.Kira = kira;
+            this.Depozito = depozito;           
+        }
+
+        public double Kira { get; set; }
+        public double Depozito { get; set; }
+
+        //public new string EvBilgileri()
+        //{
+        //    return $"{base.EvBilgileri()}\nKira:{this.Kira}\nDepozito:{this.Depozito}";
+        //}
+        public override void EvBilgisi()
+        {
+            Console.WriteLine($"Semt: {Semt}\n Oda Sayısı: {Odasayisi}\n Kat: {Katno}\n Alan: {Alan} m²\n Kira: {Kira} TL\n Depozito: {Depozito} TL");
+        }
+        public override string EvBilgileri()
+        {
+            return $"{base.EvBilgileri()}\nKira:{this.Kira}\nDepozito:{this.Depozito}";
+        }
+    }
+}
+
+//internal: Yalnızca aynı namespace içindeki classlar tarafından erişilebilir.
---
+++ Sube1.EmlakciLib/SatilikEv.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sube1.EmlakciLib
+{
+    public class SatilikEv : Ev
+    {
+        public SatilikEv()
+        {
+
+        }
+
+        public SatilikEv(double satisfiyat, int odasayisi, string semt, int katno, double alan) : base(odasayisi, katno, alan, semt)
+        {
+            Satisfiyat = satisfiyat;
+        }
+
+        public double Satisfiyat { get; set; }
+
+        //public new string EvBilgileri()
+        //{
+        //    return $"{base.EvBilgileri()}\nSatış Fiyat:{this.Satisfiyat}";
+        //}
+        public override void EvBilgisi()
+        {
+            Console.WriteLine($"Semt: {Semt}\n Oda Sayısı: {Odasayisi}\n Kat: {Katno}\n Alan: {Alan} m²\nSatış Fiyatı: {Satisfiyat} TL");
+        }
+        public override string EvBilgileri()
+        {
+            return $"{base.EvBilgileri()}\nSatış Fiyat:{this.Satisfiyat}";
+        }
+    }
+}
+
+//Name Hiding: Türeyen classlarda, base classtaki üye(ler)in ismiyle aynı isimli bir üye tanımlanırsa, artık türeyen class referansıyla base classtaki üyeye erişilemez. 
